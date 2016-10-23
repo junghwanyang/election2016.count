16e2fdbd200ffb11026370c604f62d5174ce9786
JungHwan Yang <junghwanyang@users.noreply.github.com>
2016-10-22 21:38:13 -0500

    Add files via upload
    Daily counts of @tedcruz, @sentedcruz

---
+++ tedcruzCount/cruzRT.count.3.sql
@@ -0,0 +1,42 @@
+insert overwrite local directory 'keyword.count/cruzRT.count.3'
+row format delimited
+fields terminated by '\t'
+select year, month, day, count(*)
+from gh_rc2
+where
+    year=2015
+    and
+    month>6
+    and
+    month<=9
+    and
+    --lower(text) regexp '\\scruz\\W'
+    --and
+    (
+        lower(text) like '%RT @tedcruz%'
+        or
+        lower(text) like '%RT @sentedcruz%'
+        or
+        (
+            retweeted_status.user.screen_name is not NULL
+            and
+            lower(retweeted_status.user.screen_name) like '%tedcruz%'
+        )
+        or
+        (
+            retweeted_status.user.screen_name is not NULL
+            and
+            lower(retweeted_status.user.screen_name) like '%sentedcruz%'
+        )
+    )
+group by year, month, day;
+
+-- This script counts retweets of ted cruz's tweet (RT @tedcruz, RT @SenTedCruz)
+
+-- @tedcruz
+-- @SenTedCruz
+
+-- http://www.vogella.com/tutorials/JavaRegularExpressions/article.html
+-- http://www.regexplanet.com/advanced/java/index.html
+-- http://hadooptutorial.info/hive-functions-examples/#LIKE_RLIKE
+-- https://cwiki.apache.org/confluence/display/Hive/LanguageManual+UDF
