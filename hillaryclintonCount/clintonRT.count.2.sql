40fd826d93e4fa2e30dee1caf68e91f539aae063
JungHwan Yang <junghwanyang@users.noreply.github.com>
2016-10-24 11:34:57 -0500

    Add files via upload
    Added daily counts of @hillaryclinton

---
+++ hillaryclintonCount/clintonRT.count.2.sql
@@ -0,0 +1,33 @@
+insert overwrite local directory 'keyword.count/clintonRT.count.2'
+row format delimited
+fields terminated by '\t'
+select year, month, day, count(*)
+from gh_rc2
+where
+    year=2015
+    and
+    month>3
+    and
+    month<=6
+    and
+    --lower(text) regexp '\\scruz\\W'
+    --and
+    (
+        lower(text) like '%RT @hillaryclinton%'
+        or
+        (
+            retweeted_status.user.screen_name is not NULL
+            and
+            lower(retweeted_status.user.screen_name) like '%hillaryclinton%'
+        )
+    )
+group by year, month, day;
+
+-- This script counts retweets of hillary clinton's tweet (RT @hillaryclinton)
+
+-- @hillaryclinton
+
+-- http://www.vogella.com/tutorials/JavaRegularExpressions/article.html
+-- http://www.regexplanet.com/advanced/java/index.html
+-- http://hadooptutorial.info/hive-functions-examples/#LIKE_RLIKE
+-- https://cwiki.apache.org/confluence/display/Hive/LanguageManual+UDF
---
+++ hillaryclintonCount/execute_all.clintonRT.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+for ((i=1; i <= 7 ; i++))
+do
+   hive -f hive_scripts/more.candidates/hillaryclintonCount/clintonRT.count.$(printf '%d' $i).sql
+done
