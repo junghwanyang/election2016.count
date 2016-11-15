1f9affee9540c44fc560abc94f580e25a3993e56
JungHwan Yang <junghwanyang@users.noreply.github.com>
2016-11-15 12:15:22 -0600

    Add files via upload

--- berniesanders.id_str/sandersKeyword.id_str.sql
+++ berniesanders.id_str/sandersKeyword.id_str.sql
@@ -1,4 +1,4 @@
-insert overwrite local directory 'elex/sanders.id_str.keyword'
+insert overwrite local directory 'elex2016/sanders.id_str.keyword'
 row format delimited
 fields terminated by '\t'
 select year, month, day, created_at, id_str
